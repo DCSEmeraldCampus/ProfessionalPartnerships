86943f4c49c2c6c26d55ce29fe9cb10aeed3e076
Calvin A. Allen <calvin.allen@outlook.com>
2018-03-10 00:42:25 -0500

    Added "Manage Companies" to navigation Added ability to create new company - still needs to transition to proper viewmodel instead of domain object.

--- ProfessionalPartnerships.Web/Controllers/CompaniesController.cs
+++ ProfessionalPartnerships.Web/Controllers/CompaniesController.cs
@@ -18,23 +18,43 @@ namespace ProfessionalPartnerships.Web.Controllers
 			return View(companies);
 		}
 
-		[HttpGet("{companyId}")]
+		[HttpGet("{companyId?}")]
 		public async Task<IActionResult> Edit(int companyId)
 		{
-			var company = await _db.Companies.FindAsync(companyId);
+			var company = await _db.Companies.FindAsync(companyId) ?? new Companies();
 			return View(company);
 		}
 
 		[HttpPost]
 		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> Edit(CompaniesViewModel model)
+		public async Task<IActionResult> Save([FromForm]Companies model)
 		{
-			if (ModelState.IsValid)
+			if (!ModelState.IsValid)
 			{
-
+				return View("Edit", model);
 			}
-			// If we got this far, something failed, redisplay form
-			return View(model);
+
+			//TODO: placeholder until switch to proper viewmodel
+			if (model.CompanyId != default(int))
+			{
+				//pre-existing company, query and update
+			}
+			else
+			{
+				_db.Add(new Companies
+				{
+					Name = model.Name,
+					Address1 = model.Address1,
+					Address2 = model.Address2,
+					City = model.City,
+					State = model.State,
+					Zip = model.Zip,
+					IsActive = model.IsActive
+				});
+				await _db.SaveChangesAsync();
+			}
+
+			return RedirectToAction("List");
 		}
 
 
