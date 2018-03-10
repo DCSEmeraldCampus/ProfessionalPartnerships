be35d72e088be644567e99a5151be1c11c2359ee
Baskar Rao <bdandlamudi@affiniongroup.com>
2018-03-10 11:39:32 -0500

    merged calvin changes

--- ProfessionalPartnerships.Web/Controllers/AdminController.cs
+++ ProfessionalPartnerships.Web/Controllers/AdminController.cs
@@ -21,61 +21,7 @@ namespace ProfessionalPartnerships.Web.Controllers
             _dbContext = db;
         }
 
-        [HttpGet]        
-        public async Task<IActionResult> ManageCompanies()
-        {
-           
-            return View();
-
-        }
-
-        [HttpPost]
-        [AllowAnonymous]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> ManageCompanies(CompaniesViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                _dbContext.Add(new Companies
-                {
-                    Name = model.Name,
-                    Address1 = model.Address1,
-                    Address2 = model.Address2,
-                    City = model.City,
-                    State = model.State,
-                    Zip = model.Zip,
-                    IsActive = model.IsActive
-                });
-                _dbContext.SaveChanges();
-                return View(model);
-            }
-            // If we got this far, something failed, redisplay form
-            return View(model);
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> ViewCompanies()
-        {
-            List<CompaniesViewModel> result = new List<CompaniesViewModel>();
-            var companies = _dbContext.Companies;
-            result.Add(new CompaniesViewModel { Name = "Test", Address1 = "Test1", Address2 = "Test2", City = "dublin", State = "Ohio", Zip = "23123", IsActive = true });
-            //foreach(var company in companies)
-            //{
-            //    result.Add(new CompaniesViewModel
-            //    {
-            //        Name = company.Name,
-            //        Address1 = company.Address1,
-            //        Address2 = company.Address2,
-            //        City = company.City,
-            //        State = company.State,
-            //        Zip = company.Zip,
-            //        IsActive = company.IsActive
-            //    });
-            //}
-            
-            return  View(result);
-        }
-
+        
         [HttpGet]
         public async Task<IActionResult> ManagePrograms()
         {
---
+++ ProfessionalPartnerships.Web/Models/AdminViewModels/Companies/CompaniesViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProfessionalPartnerships.Web.Models.AdminViewModels.Companies
+{
+    public class CompaniesViewModel
+    {
+        public int CompanyId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [DisplayName("Address 1")]
+        public string Address1 { get; set; }
+
+        [DisplayName("Address 2")]
+        public string Address2 { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string State { get; set; }
+        [Required]
+        public string Zip { get; set; }
+
+        [DisplayName("Active?")]
+        public bool IsActive { get; set; }
+
+        [DisplayName("Primary Professional")]
+        public int? PrimaryProfessionalId { get; set; }
+
+        public CompaniesViewModel()
+        {
+
+        }
+
+        public CompaniesViewModel(ProfessionalPartnerships.Data.Models.Companies company)
+        {
+            this.CompanyId = company.CompanyId;
+            this.Name = company.Name;
+            this.Address1 = company.Address1;
+            this.Address2 = company.Address2;
+            this.City = company.City;
+            this.State = company.State;
+            this.Zip = company.Zip;
+            this.IsActive = company.IsActive;
+            this.PrimaryProfessionalId = company.PrimaryProfessionalId;
+        }
+
+        public void ApplyTo(ProfessionalPartnerships.Data.Models.Companies company)
+        {
+            company.Name = this.Name;
+            company.Address1 = this.Address1;
+            company.Address2 = this.Address2;
+            company.City = this.City;
+            company.State = this.State;
+            company.Zip = this.Zip;
+            company.IsActive = this.IsActive;
+            company.PrimaryProfessionalId = this.PrimaryProfessionalId;
+        }
+    }
+}
