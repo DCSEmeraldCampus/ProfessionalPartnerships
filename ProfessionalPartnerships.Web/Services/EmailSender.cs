08e8012e292da60cd90df627cb19c49632091ce9
Chris Berry <cberry@hmbnet.com>
2018-03-09 20:39:45 -0500

    Services layer

--- ProfessionalPartnerships.Web/Services/EmailSender.cs
+++ ProfessionalPartnerships.Web/Services/EmailSender.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ProfessionalPartnerships.Web.Interface;
 
 namespace ProfessionalPartnerships.Web.Services
 {
