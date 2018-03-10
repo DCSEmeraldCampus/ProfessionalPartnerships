5123f7f015a9dc9318849a217f8a8c964d58012f
Calvin A. Allen <calvin.allen@outlook.com>
2018-03-10 17:25:15 -0500

    Stubbed out a basic Enrollment list view

---
+++ ProfessionalPartnerships.Web/Controllers/EnrollmentsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProfessionalPartnerships.Data.Models;
+
+namespace ProfessionalPartnerships.Web.Controllers
+{
+    public class EnrollmentsController : BaseController
+    {
+	    public EnrollmentsController(PartnershipsContext db) : base(db) { }
+
+	    public async Task<IActionResult> List()
+	    {
+		    var enrollments = await _db.Enrollments
+			    .Include(e => e.Student)
+				.Include(e => e.EnrollmentStatus)
+				.Include(e => e.Program)
+				.Include(p => p.Program.PointOfContactProfessional)
+			    .Include(p => p.Program.PointOfContactProfessional.Company)
+				.ToListAsync();
+
+		    return View(enrollments);
+	    }
+    }
+}
