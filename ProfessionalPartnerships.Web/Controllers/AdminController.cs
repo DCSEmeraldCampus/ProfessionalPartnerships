90ff7e696d04ee14e3ad1c6f6a16182f40061be4
Calvin A. Allen <calvin.allen@outlook.com>
2018-03-09 23:24:38 -0500

    Moved the Company administration to a separate controller and stubbed out/moved views

--- ProfessionalPartnerships.Web/Controllers/AdminController.cs
+++ ProfessionalPartnerships.Web/Controllers/AdminController.cs
@@ -7,31 +7,13 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using ProfessionalPartnerships.Web.Models.AdminViewModels;
+
 namespace ProfessionalPartnerships.Web.Controllers
 {
-   
-    [Route("[controller]/[action]")]
+	[Route("[controller]/[action]")]
     public class AdminController : Controller
     {
-        [HttpGet]        
-        public async Task<IActionResult> ManageCompanies()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        [AllowAnonymous]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> ManageCompanies(CompaniesViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-              
-            }
-            // If we got this far, something failed, redisplay form
-            return View(model);
-        }
-
+        
         [HttpGet]
         public async Task<IActionResult> ManagePrograms()
         {
---
+++ ProfessionalPartnerships.Web/Controllers/CompaniesController.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ProfessionalPartnerships.Web.Models.AdminViewModels;
+
+namespace ProfessionalPartnerships.Web.Controllers
+{
+	[Route("admin/[controller]/[action]")]
+	public class CompaniesController : Controller
+	{
+		[HttpGet]
+		public async Task<IActionResult> List()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Edit(int id)
+		{
+			if (ModelState.IsValid)
+			{
+
+			}
+			// If we got this far, something failed, redisplay form
+			return View();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(CompaniesViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+
+			}
+			// If we got this far, something failed, redisplay form
+			return View(model);
+		}
+
+	}
+}
