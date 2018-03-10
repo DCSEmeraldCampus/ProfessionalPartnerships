5d2844f55d51165e5b45fb84caa01ded422a0e21
baskarrao <baskarrao.dandlamudi@outlook.com>
2018-03-10 14:36:40 -0500

    AdminDashBoard Changes

--- ProfessionalPartnerships.Web/Controllers/AdministratorDashboardController.cs
+++ ProfessionalPartnerships.Web/Controllers/AdministratorDashboardController.cs
@@ -1,13 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ProfessionalPartnerships.Data.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using ProfessionalPartnerships.Web.Models;
 
 namespace ProfessionalPartnerships.Web.Controllers
 {
     public class AdministratorDashboardController : BaseController
     {
-        public AdministratorDashboardController(PartnershipsContext db) : base(db)
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AdministratorDashboardController(PartnershipsContext db,
+            UserManager<ApplicationUser> userManager) : base(db)
         {
+            _userManager = userManager;
         }
+
+        [Authorize(Roles = "Administrator")]
         public ActionResult Index()
         {
             return View();
