ba846bd0c8bd53ce12319936cb70c912c5618ea7
joebo <joebogner@gmail.com>
2018-03-10 16:35:08 -0500

    as a student - apply to a program

---
+++ ProfessionalPartnerships.Web/Models/DashboardViewModels/StudentDashboardViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ProfessionalPartnerships.Web
+{
+    public class StudentDashboardViewModel
+    {
+        public string description { get; set; }
+        public int maximumStudentCount { get; set; }
+        public string programTypeName { get; set; }
+        public int programId { get; set; }
+        public int enrolledCount { get; set;  }
+        public string enrollmentStatus { get; set; }
+    }
+}
