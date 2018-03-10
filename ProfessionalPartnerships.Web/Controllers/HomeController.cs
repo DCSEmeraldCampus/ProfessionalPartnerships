926444457cc3dbe02d9d5e651a34b8fd8784fce6
Rick Kierner <rkierner@igsenergy.com>
2018-03-09 21:40:04 -0500

    Working EmailSender

--- ProfessionalPartnerships.Web/Controllers/HomeController.cs
+++ ProfessionalPartnerships.Web/Controllers/HomeController.cs
@@ -4,12 +4,20 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using ProfessionalPartnerships.Web.Models;
+using ProfessionalPartnerships.Web.Services;
 
 namespace ProfessionalPartnerships.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         public IActionResult Index()
         {
             return View();
@@ -33,5 +41,13 @@ namespace ProfessionalPartnerships.Web.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        //public async Task<IActionResult> SendEmail()
+        //{
+        //    EmailSender sender = new EmailSender(_configuration);
+        //    await sender.SendEmailAsync("rick@rickdoes.net", "Dublin Emerald Campus Email Test",
+        //        "<p>This is your test email</p>");
+        //    return StatusCode(200);
+        //}
     }
 }
