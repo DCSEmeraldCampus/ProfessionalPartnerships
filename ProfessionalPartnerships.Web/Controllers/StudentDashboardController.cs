88bc91deabd95a158657f508aafc4a1195caca35
Joe Bogner <jbogner@d3advantage.com>
2018-03-09 22:42:31 -0500

    add BaseController and start of Student Dashboard

---
+++ ProfessionalPartnerships.Web/Controllers/StudentDashboardController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProfessionalPartnerships.Data.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProfessionalPartnerships.Web.Controllers
+{
+    public class StudentDashboardController : BaseController
+    {
+        
+        public StudentDashboardController(PartnershipsContext db) : base(db)
+        {
+
+        }
+
+        public ActionResult Index()
+        {          
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult GetPrograms()
+        {
+            return Json(Database.Programs.ToList());
+        }
+    }
+}
