3b1ab6cbcb61183c864564daa54061e5bb20665b
Calvin A. Allen <calvin.allen@outlook.com>
2018-03-10 22:45:58 -0500

    Added the fully functional List and Edit screens for Enrollments, and a quick access button to the dashboard when not logged in

--- ProfessionalPartnerships.Web/Models/AdminViewModels/Enrollments/EnrollmentsListViewModel.cs
+++ ProfessionalPartnerships.Web/Models/AdminViewModels/Enrollments/EnrollmentsListViewModel.cs
@@ -1,21 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace ProfessionalPartnerships.Web.Models.AdminViewModels.Enrollments
 {
     public class EnrollmentsListViewModel : List<EnrollmentsViewModel>
     {
-	    public IEnumerable<EnrollmentsViewModel> Applied
-		    => this.Where(x => x.EnrollmentStatusId == 1);
-
-	    public IEnumerable<EnrollmentsViewModel> Approved
-		    => this.Where(x => x.EnrollmentStatusId == 2);
-
-	    public IEnumerable<EnrollmentsViewModel> Declined
-		    => this.Where(x => x.EnrollmentStatusId == 3);
-
 		public EnrollmentsListViewModel(IEnumerable<ProfessionalPartnerships.Data.Models.Enrollments> enrollments)
 	    {
 		    foreach (var enrollment in enrollments)
