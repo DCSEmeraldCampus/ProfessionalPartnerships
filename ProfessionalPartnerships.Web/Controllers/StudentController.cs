8b31812c7c2e2b0ac879075a67001d83b2cfa014
crazyman974 <nope@nope.com>
2018-03-11 17:27:20 -0400

    refactoring...

--- ProfessionalPartnerships.Web/Controllers/StudentController.cs
+++ ProfessionalPartnerships.Web/Controllers/StudentController.cs
@@ -1,28 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
-using ProfessionalPartnerships.Web.Models.AdminViewModels;
-using ProfessionalPartnerships.Data.Models;
-using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using ProfessionalPartnerships.Data.Models;
 using ProfessionalPartnerships.Web.Models;
-using ProfessionalPartnerships.Web.Models.DashboardViewModels;
 using ProfessionalPartnerships.Web.Models.StudentViewModels;
 
 namespace ProfessionalPartnerships.Web.Controllers
 {
-
     [Route("[controller]/[action]")]
     public class StudentController : BaseController
     {
         private const string ApprovedStatus = "Approved";
 
         private readonly UserManager<ApplicationUser> _userManager;
+
         public StudentController(PartnershipsContext db,
             UserManager<ApplicationUser> userManager) : base(db)
         {
@@ -34,13 +28,11 @@ namespace ProfessionalPartnerships.Web.Controllers
         private int GetCurrentStudentId()
         {
             string userId = null;
-            if (HttpContext.User.Identity != null && !String.IsNullOrEmpty(HttpContext.User.Identity.Name))
+            if (HttpContext.User.Identity != null && !string.IsNullOrEmpty(HttpContext.User.Identity.Name))
             {
                 var user = GetCurrentUser();
                 if (user != null)
-                {
                     userId = user.Id;
-                }
             }
 
             //for testing
@@ -49,9 +41,7 @@ namespace ProfessionalPartnerships.Web.Controllers
             {
                 var myStudent = Database.Students.Where(x => x.AspNetUserId == userId).FirstOrDefault();
                 if (myStudent != null)
-                {
                     return myStudent.StudentId;
-                }
             }
             return 0;
         }
@@ -66,7 +56,7 @@ namespace ProfessionalPartnerships.Web.Controllers
         [HttpGet]
         public ViewResult Programs()
         {
-            int studentId = GetCurrentStudentId();
+            var studentId = GetCurrentStudentId();
 
             var myEnrollments = new Dictionary<int, string>();
             var myReviews = new Dictionary<int, int>();
@@ -74,19 +64,18 @@ namespace ProfessionalPartnerships.Web.Controllers
             if (studentId != 0)
             {
                 myEnrollments = Database.Enrollments
-                   .Include(e => e.EnrollmentStatus)
-                   .Where(x => x.StudentId == studentId)
-                   .ToDictionary(w => w.ProgramId, w => w.EnrollmentStatus.Name);
+                    .Include(e => e.EnrollmentStatus)
+                    .Where(x => x.StudentId == studentId)
+                    .ToDictionary(w => w.ProgramId, w => w.EnrollmentStatus.Name);
 
                 myReviews = Database.StudentReviews
                     .Where(w => w.StudentId == studentId)
-                    .ToDictionary(w => w.ProgramId, w => (int)w.Stars);
-
+                    .ToDictionary(w => w.ProgramId, w => (int) w.Stars);
             }
 
             var enrollmentTotals = Database.Enrollments.GroupBy(x => x.ProgramId).Select(g => new
             {
-                ProgramId = g.First().ProgramId,
+                g.First().ProgramId,
                 EnrolledCount = g.Count(x => x.EnrollmentStatus.IsDisregardedInEnrollmentCount == false)
             });
 
@@ -96,37 +85,38 @@ namespace ProfessionalPartnerships.Web.Controllers
                 .Where(p => p.IsActive)
                 .GroupJoin(enrollmentTotals,
                     p => p.ProgramId,
-                    e => e.ProgramId, (p, e) => new { Program = p, Enrollments = e }).SelectMany(
+                    e => e.ProgramId, (p, e) => new {Program = p, Enrollments = e}).SelectMany(
                     e => e.Enrollments.Select(x => x.EnrolledCount).DefaultIfEmpty(),
-                (p, ct) => new ProgramViewModel
-                {
-                    Description = p.Program.Description,
-                    MaximumStudentCount = p.Program.MaximumStudentCount,
-                    ProgramTypeName = p.Program.ProgramType.Name,
-                    ProgramId = p.Program.ProgramId,
-                    EnrolledCount = ct,
-                    EnrollmentStatus = myEnrollments.ContainsKey(p.Program.ProgramId) ? myEnrollments[p.Program.ProgramId] : "",
-                    SemesterName = p.Program.Semester.Name,
-                    Stars = myReviews.ContainsKey(p.Program.ProgramId) ? (int?)myReviews[p.Program.ProgramId] : null,
-                    ProgramAvailableToReview = p.Program.EndDate < DateTime.Now
-                        && myEnrollments.ContainsKey(p.Program.ProgramId)
-                        && myEnrollments[p.Program.ProgramId] == ApprovedStatus
-                        && myReviews.ContainsKey(p.Program.ProgramId) == false
-                });
+                    (p, ct) => new ProgramViewModel
+                    {
+                        Description = p.Program.Description,
+                        MaximumStudentCount = p.Program.MaximumStudentCount,
+                        ProgramTypeName = p.Program.ProgramType.Name,
+                        ProgramId = p.Program.ProgramId,
+                        EnrolledCount = ct,
+                        EnrollmentStatus = myEnrollments.ContainsKey(p.Program.ProgramId)
+                            ? myEnrollments[p.Program.ProgramId]
+                            : "",
+                        SemesterName = p.Program.Semester.Name,
+                        Stars = myReviews.ContainsKey(p.Program.ProgramId)
+                            ? (int?) myReviews[p.Program.ProgramId]
+                            : null,
+                        ProgramAvailableToReview = p.Program.EndDate < DateTime.Now
+                                                   && myEnrollments.ContainsKey(p.Program.ProgramId)
+                                                   && myEnrollments[p.Program.ProgramId] == ApprovedStatus
+                                                   && myReviews.ContainsKey(p.Program.ProgramId) == false
+                    });
 
             return View(rows);
         }
 
         public IActionResult Review(int programId, string note, int stars)
         {
-            int studentId = GetCurrentStudentId();
+            var studentId = GetCurrentStudentId();
             if (studentId == 0) throw new ApplicationException("Note logged in");
 
-            var existing = Database.StudentReviews.FirstOrDefault(x => x.StudentId == studentId && x.ProgramId == programId);
-            if (existing != null)
-            {
+            if (Database.StudentReviews.Any(x => x.StudentId == studentId && x.ProgramId == programId))
                 throw new ApplicationException("Already reviewed program");
-            }
 
             Database.StudentReviews.Add(new StudentReviews
             {
@@ -143,14 +133,12 @@ namespace ProfessionalPartnerships.Web.Controllers
 
         public IActionResult ApplyProgram(int programId, string note)
         {
-            int studentId = GetCurrentStudentId();
-            if (studentId == 0) throw new ApplicationException("Not Logged In"); ;
+            var studentId = GetCurrentStudentId();
+            if (studentId == 0) throw new ApplicationException("Not Logged In");
+            ;
 
-            var existing = Database.Enrollments.Where(x => x.StudentId == studentId && x.ProgramId == programId).FirstOrDefault();
-            if (existing != null)
-            {
+            if (Database.Enrollments.Any(x => x.StudentId == studentId && x.ProgramId == programId))
                 throw new ApplicationException("Already Applied");
-            }
 
             Database.Enrollments.Add(new Enrollments
             {
@@ -162,7 +150,7 @@ namespace ProfessionalPartnerships.Web.Controllers
 
             Database.SaveChanges();
 
-  
+
             return RedirectToAction("Programs");
         }
 
