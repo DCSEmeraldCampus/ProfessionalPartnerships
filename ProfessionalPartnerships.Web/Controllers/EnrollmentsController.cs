0b598727c8a3e2b3665b708f412a33c86d23cc04
Calvin A. Allen <calvin.allen@outlook.com>
2018-03-10 19:14:12 -0500

    List of all enrollments.  You can Approve or Deny somebody who is currenlty an applicant, deny someone previously approved, or approve someone previously denied

--- ProfessionalPartnerships.Web/Controllers/EnrollmentsController.cs
+++ ProfessionalPartnerships.Web/Controllers/EnrollmentsController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ProfessionalPartnerships.Data.Models;
+using ProfessionalPartnerships.Web.Models.AdminViewModels.Enrollments;
 
 namespace ProfessionalPartnerships.Web.Controllers
 {
@@ -12,17 +14,38 @@ namespace ProfessionalPartnerships.Web.Controllers
     {
 	    public EnrollmentsController(PartnershipsContext db) : base(db) { }
 
+		[HttpGet]
 	    public async Task<IActionResult> List()
 	    {
 		    var enrollments = await _db.Enrollments
 			    .Include(e => e.Student)
 				.Include(e => e.EnrollmentStatus)
 				.Include(e => e.Program)
+				.Include(e => e.Program.ProgramType)
 				.Include(p => p.Program.PointOfContactProfessional)
 			    .Include(p => p.Program.PointOfContactProfessional.Company)
 				.ToListAsync();
 
-		    return View(enrollments);
+		    var viewModel = new EnrollmentsListViewModel(enrollments);
+
+		    return View(viewModel);
 	    }
-    }
+
+	    [HttpPost]
+	    public async Task<IActionResult> UpdateEnrollmentStatus(int enrollmentId, int newStatusId)
+	    {
+		    var enrollment = await _db.Enrollments.FindAsync(enrollmentId);
+
+		    if (enrollment == null)
+		    {
+			    return StatusCode(404, $"Unable to locate Enrollment record #{enrollmentId}");
+		    }
+
+		    enrollment.EnrollmentStatusId = newStatusId;
+
+		    await _db.SaveChangesAsync();
+
+			return StatusCode(200);
+	    }
+	}
 }
